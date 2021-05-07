3ecb070dab7d2a3d4fbf97af21ca61df3311ca43
germanseoane <german@rumbosturismo>
2021-05-07 18:08:48 -0300

    user token changed

--- models/user.js
+++ models/user.js
@@ -26,7 +26,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  const token = jwt.sign(
+    { _id: this._id, email: this.email, name: this.name },
+    config.get("jwtPrivateKey")
+  );
   return token;
 };
 
